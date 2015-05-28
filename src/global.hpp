e209770a054b9eb8ac2959de94a00f806acc03e9
Nephet <b.vidot@rubika-edu.com>
2015-05-28 15:40:09 +0200

    title

--- src/global.hpp
+++ src/global.hpp
@@ -7,7 +7,7 @@
 #define MAX_FPS 60
 #define MIN_FPS 20
 #define MAX_DT 1.0f/MIN_FPS
-#define APP_NAME "Fibonacci Simulator 2014"
+#define APP_NAME "Fat Labrador Simulator 2014"
 
 namespace global
 {
